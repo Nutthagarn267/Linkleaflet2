c39943c9fae5b89cf86e8a18ad011d23662f4a35
Nutthagarn267 <124670673+Nutthagarn267@users.noreply.github.com>
2023-04-25 15:54:06 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "himai_historical_park",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.49353857534516,
+            15.21890363172578
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat_Sala_Loi",
+          "type": "culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.11632592908325,
+            14.98011146541647
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao_Yai_Thiang",
+          "type": "nature",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.55460355734544,
+            14.784814710925843
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wihan_Thep_Wittayakom",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.73592541324103,
+            15.297434327727117
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ya_Mo_Monument",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.11714210551582,
+            14.98093160009546
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Manesorn_Farm",
+          "type": "nature",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.44522006616074,
+            14.573000966295368
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao_Yai",
+          "type": "nature",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.3785882711627,
+            14.534261537604436
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat_Luang_Phor_To",
+          "type": "culture",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            101.73517927116006,
+            14.873693926320882
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kep_Tawan_Cliff",
+          "type": "nature",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            101.93038141604092,
+            14.318406372030381
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Jim_Thompson_Farm",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            101.86711127116439,
+            14.642663782987555
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
+  
